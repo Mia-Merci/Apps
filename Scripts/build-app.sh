ce89ef5226c5fa660e4d172345bfe1e95cb1820e
Mia-Merci <173337609+Mia-Merci@users.noreply.github.com>
2026-09-05 19:36:56 +0800

    Add app icon to release bundle

--- Scripts/build-app.sh
+++ Scripts/build-app.sh
@@ -7,6 +7,8 @@ cache_dir="$package_root/.build/cache"
 app_dir="$package_root/outputs/CodexQuotaBar.app"
 contents_dir="$app_dir/Contents"
 macos_dir="$contents_dir/MacOS"
+resources_dir="$contents_dir/Resources"
+icon_path="$package_root/Resources/CodexQuotaBar.icns"
 
 cd "$package_root"
 mkdir -p "$cache_dir/clang-module-cache" "$cache_dir/swiftpm-module-cache"
@@ -15,14 +17,17 @@ export SWIFTPM_MODULECACHE_OVERRIDE="$cache_dir/swiftpm-module-cache"
 swift build --disable-sandbox -c release
 
 rm -rf "$app_dir"
-mkdir -p "$macos_dir"
+mkdir -p "$macos_dir" "$resources_dir"
 cp "$build_dir/CodexQuotaBar" "$macos_dir/CodexQuotaBar"
+cp "$icon_path" "$resources_dir/CodexQuotaBar.icns"
 
 cat > "$contents_dir/Info.plist" <<'PLIST'
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
+  <key>CFBundleIconFile</key>
+  <string>CodexQuotaBar</string>
   <key>CFBundleExecutable</key>
   <string>CodexQuotaBar</string>
   <key>CFBundleIdentifier</key>
